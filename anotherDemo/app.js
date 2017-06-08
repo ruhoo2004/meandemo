3adc3855fa6a07ee1351a001c532dd0b5b38362c
ru hong <ruhoo2004@gmail.com>
2017-06-08 11:21:18 +0800

    update

--- anotherDemo/app.js
+++ anotherDemo/app.js
@@ -4,9 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/test')
+    .then(() => console.log('connected to DB'))
+    .catch((err) => console.error(err));
 
 var app = express();
 
--- anotherDemo/routes/index.js
+++ anotherDemo/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var user = require('../model/user.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/helloworld', function(req, res, next) {
+  res.render('helloworld', { title: 'Hello World' });
+});
+
+router.get('/userlist', function(req, res, next) {
+    user.find(function(err, results) {
+        if (err) return next(err);
+        res.render('userlist', {"users" : results});
+    });
 });
 
 module.exports = router;
